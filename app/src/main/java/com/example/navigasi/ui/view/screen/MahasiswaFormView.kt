24585e1060ea1e21cc792c10a4136551992f6472
ijaamm <ijammusyaf@gmail.com>
2024-11-22 16:39:30 +0700

    membuat function dan menambah beberapa variable

---
+++ app/src/main/java/com/example/navigasi/ui/view/screen/MahasiswaFormView.kt
@@ -0,0 +1,59 @@
+package com.example.navigasi.ui.view.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigasi.R
+import androidx.compose.material3.Text as Text
+
+@Composable
+fun MahasiswaFormView(
+    modifier: Modifier,
+    onSubmitButtonCliked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+    var nama by remember {
+        mutableStateOf("")
+    }
+    var nim by remember {
+        mutableStateOf("")
+    }
+    var email by remember {
+        mutableStateOf("")
+    }
+    val listData: MutableList<String> =
+        mutableListOf(nama, nim, email)
+
+
+}
