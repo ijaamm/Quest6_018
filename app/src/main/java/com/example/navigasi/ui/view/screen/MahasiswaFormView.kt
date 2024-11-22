6287b73f9b88951f68981218c3ddb7239e102f58
ijaamm <ijammusyaf@gmail.com>
2024-11-22 16:49:42 +0700

    membuat box yang isinya column dan ada beberapa text

--- app/src/main/java/com/example/navigasi/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/navigasi/ui/view/screen/MahasiswaFormView.kt
@@ -91,6 +91,37 @@ fun MahasiswaFormView(
                 )
             }
         }
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+                .fillMaxSize(),
+        ){
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(
+                    text = "Masukkan Data Kamu",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 19.sp
+                )
+                Text(
+                    text = "Isi sesuai data yang kamu daftarkan",
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
 
+
+            }
+        }
     }
 }
