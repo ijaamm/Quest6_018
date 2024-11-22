24bf2c0e1e51f5a50193b5d4b795d915ae5d60d6
ijaamm <ijammusyaf@gmail.com>
2024-11-22 16:32:47 +0700

    membuat function dynamicselectfield dan membuat var expanded

---
+++ app/src/main/java/com/example/navigasi/ui/widget/DynamicSelectField.kt
@@ -0,0 +1,31 @@
+package com.example.navigasi.ui.widget
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectField(
+    selectValue: String,
+    options: List<String>,
+    label: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+}
